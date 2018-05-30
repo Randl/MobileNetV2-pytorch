Fix average pooling calculation

model.py
@@ -87,7 +87,7 @@ class MobileNet2(nn.Module):
         self.num_classes = num_classes
 
         self.num_of_channels = [32, 16, 24, 32, 64, 96, 160, 320]
-        self.last_pooling = {224: 7, 192: 6, 160: 5, 128: 4, 96: 3}
+        assert (input_size % 32 == 0)
 
         self.c = [_make_divisible(ch * self.scale, 8) for ch in self.num_of_channels]
         self.n = [1, 1, 2, 3, 4, 3, 3, 1]
@@ -97,11 +97,10 @@ class MobileNet2(nn.Module):
         self.bottlenecks = self._make_bottlenecks()
 
         # Last convolution has 1280 output channels for scale <= 1
-        # TODO: check
         self.last_conv_out_ch = 1280 if self.scale <= 1 else _make_divisible(1280 * self.scale, 8)
         self.conv_last = nn.Conv2d(self.c[-1], self.last_conv_out_ch, kernel_size=1, bias=False)
         self.bn_last = nn.BatchNorm2d(self.last_conv_out_ch)
-        self.avgpool = nn.AvgPool2d(self.last_pooling[input_size])
+        self.avgpool = nn.AvgPool2d(input_size // 32)
         self.dropout = nn.Dropout(p=0.2, inplace=True)  # confirmed by paper authors
         self.fc = nn.Linear(self.last_conv_out_ch, self.num_classes)
         self.init_params()
@@ -188,8 +187,11 @@ if __name__ == "__main__":
     print(model3)
     x = torch.randn(1, 2, 224, 224)
     print(model3(x))
-    model4_size = 192
+    model4_size = 32 * 10
     model4 = MobileNet2(input_size=model4_size, num_classes=10)
     print(model4)
     x2 = torch.randn(1, 3, model4_size, model4_size)
     print(model4(x2))
+    model5 = MobileNet2(input_size=196, num_classes=10)
+    x3 = torch.randn(1, 3, 196, 196)
+    print(model5(x3))  # fail